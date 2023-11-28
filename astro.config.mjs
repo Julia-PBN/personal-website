7605b958632479c8965569e38f5ef387053a48c1
Julia-PBN <julia.phambanien@gmail.com>
2023-11-28 22:24:34 +0100

    added cloudflare package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import Icons from 'unplugin-icons/vite';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-
   vite: {
-    plugins: [
-      Icons({
-        compiler: 'astro',
-      }),
-    ],
+    plugins: [Icons({
+      compiler: 'astro'
+    })]
   },
+  output: "server",
+  adapter: cloudflare()
 });
-
-
